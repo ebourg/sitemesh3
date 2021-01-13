35678544e38c65242b51fb63b00de6e017210d04
Emmanuel Bourg <ebourg@apache.org>
2021-01-13 19:20:45 +0100

    Use the body of the <sitemesh:write> tag as the default value

--- sitemesh/src/main/java/org/sitemesh/content/tagrules/decorate/SiteMeshWriteRule.java
+++ sitemesh/src/main/java/org/sitemesh/content/tagrules/decorate/SiteMeshWriteRule.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 /**
  * Replaces tags that look like {@code <sitemesh:write property='foo'/>} with the
  * {@link ContentProperty} being merged into the current document. The body contents of the tag will be
- * discarded. To get child properties, use a dot notation, e.g. {@code foo.child.grandchild}.
+ * used the default value. To get child properties, use a dot notation, e.g. {@code foo.child.grandchild}.
  *
  * @author Joe Walnes
  * @see SiteMeshContext#getContentToMerge()
@@ -19,6 +19,7 @@ import java.io.IOException;
 public class SiteMeshWriteRule extends BasicBlockRule {
 
     private final SiteMeshContext siteMeshContext;
+    private boolean undefined;
 
     public SiteMeshWriteRule(SiteMeshContext siteMeshContext) {
         this.siteMeshContext = siteMeshContext;
@@ -31,6 +32,7 @@ public class SiteMeshWriteRule extends BasicBlockRule {
         if (contentToMerge != null) {
             ContentProperty property = getProperty(contentToMerge, propertyPath);
             property.writeValueTo(tagProcessorContext.currentBuffer());
+            undefined = property.getValue() == null || property.getValue().trim().length() == 0;
         }
         tagProcessorContext.pushBuffer();
         return null;
@@ -48,7 +50,7 @@ public class SiteMeshWriteRule extends BasicBlockRule {
     protected void processEnd(Tag tag, Object data) throws IOException {
         CharSequence defaultContents = tagProcessorContext.currentBufferContents();
         tagProcessorContext.popBuffer();
-        if (siteMeshContext.getContentToMerge() == null) {
+        if (siteMeshContext.getContentToMerge() == null || undefined) {
             tagProcessorContext.currentBuffer().append(defaultContents);
         }
     }
